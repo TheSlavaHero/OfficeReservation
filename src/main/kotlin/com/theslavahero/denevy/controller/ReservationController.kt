c5dc0ac8b46f8575145f833906675589a25c8617
theslavahero <theslavahero@gmail.com>
2022-04-28 15:57:00 +0200

    updated ReservationController.kt

--- src/main/kotlin/com/theslavahero/denevy/controller/ReservationController.kt
+++ src/main/kotlin/com/theslavahero/denevy/controller/ReservationController.kt
@@ -1,20 +1,35 @@
 package com.theslavahero.denevy.controller
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.theslavahero.denevy.entity.Reservation
+import com.theslavahero.denevy.entity.repository.ReservationRepository
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 /**
  * Created by theslavahero on 26.04.22
  */
 @RestController
 @RequestMapping("/api/reservation")
-class ReservationController {
+class ReservationController(val reservationRepository: ReservationRepository) {
 
-    //create and save reservation//POST
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createReservation(@RequestBody reservation: Reservation) {//check if time for the office is available
+        reservationRepository.save(reservation)
+    }
 
     //delete reservation//DELETE
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun deleteReservation(@PathVariable id: Long) {
+        reservationRepository.deleteById(id)
+    }
 
     //get info about reservation//GET
-
+    @GetMapping("/get/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun getReservation(@PathVariable id: Long) {
+        reservationRepository.getById(id)
+    }
     //think about all possible exceptions
 }
