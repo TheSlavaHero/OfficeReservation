38e441fe06eaf0b20e9a709736d3280676a36e7f
theslavahero <theslavahero@gmail.com>
2022-05-01 22:12:09 +0200

    added methods to the ReservationRepository

--- src/main/kotlin/com/theslavahero/denevy/entity/repository/ReservationRepository.kt
+++ src/main/kotlin/com/theslavahero/denevy/entity/repository/ReservationRepository.kt
@@ -3,9 +3,24 @@ package com.theslavahero.denevy.entity.repository
 import com.theslavahero.denevy.entity.Reservation
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
 
 /**
  * Created by theslavahero on 26.04.22
  */
 @Repository
-interface ReservationRepository: JpaRepository<Reservation, Long>
+interface ReservationRepository : JpaRepository<Reservation, Long> {
+    //finds all reservations (for current Office by id) that start between dateStart and dateFinish
+    fun findAllByOfficeIdAndReservationStartAfterAndReservationStartBefore(
+        office_id: Long,
+        dateStart: LocalDateTime,
+        dateFinish: LocalDateTime
+    ): List<Reservation>
+
+    //finds all reservations (for current Office by id) that finish between dateStart and dateFinish
+    fun findAllByOfficeIdAndReservationFinishAfterAndReservationFinishBefore(
+        office_id: Long,
+        dateStart: LocalDateTime,
+        dateFinish: LocalDateTime
+    ): List<Reservation>
+}
