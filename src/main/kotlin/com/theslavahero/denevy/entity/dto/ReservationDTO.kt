2b4c2c8399369469f9489aaea0c47044003ab659
theslavahero <theslavahero@gmail.com>
2022-05-01 18:36:10 +0200

    ReservationDTO.kt change to localDateTime

--- src/main/kotlin/com/theslavahero/denevy/entity/dto/ReservationDTO.kt
+++ src/main/kotlin/com/theslavahero/denevy/entity/dto/ReservationDTO.kt
@@ -1,17 +1,24 @@
 package com.theslavahero.denevy.entity.dto
 
 import com.theslavahero.denevy.entity.Reservation
-import java.util.*
+import java.time.LocalDateTime
 
 /**
  * Created by theslavahero on 28.04.22
  */
 class ReservationDTO(
-    var officeId: Long,
-    var userId: Long,
-    var reservationStart: Date,
-    var reservationFinish: Date
+    var officeId: Long?,
+    var userId: Long?,
+    var reservationStart: LocalDateTime,
+    var reservationFinish: LocalDateTime
 ) : DTO(), Convertable<Reservation> {
+    constructor(reservationStart: LocalDateTime, reservationFinish: LocalDateTime) : this(
+        null,
+        null,
+        reservationStart,
+        reservationFinish
+    )
+
     override fun convert(): Reservation {
         TODO("Not yet implemented")
     }
